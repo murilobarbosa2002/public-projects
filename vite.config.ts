c1943debdd03d537cd5d4b5609c9a13808b5d2a7
murilobarbosa2002 <murilo.barbosa2609@gmail.com>
2022-06-16 20:24:19 -0300

    Edit router.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/public-projects/',
+  base: 'https://murilobarbosa2002.github.io/public-projects/',
   plugins: [react()]
 })
