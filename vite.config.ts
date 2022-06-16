873b804854b7c80e9fe551601533f29aea60d6c3
murilobarbosa2002 <murilo.barbosa2609@gmail.com>
2022-06-16 19:24:32 -0300

    Config Github pagees

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/github-pages/',
+  base: 'https://murilobarbosa2002.github.io/public-projects/',
   plugins: [react()]
 })
