8aed8b3fd47e34e64c97417a33a54a7e57f00bc2
murilobarbosa2002 <murilo.barbosa2609@gmail.com>
2022-06-16 19:33:47 -0300

    Routes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://murilobarbosa2002.github.io/public-projects/',
+  base: '/public-projects/',
   plugins: [react()]
 })
