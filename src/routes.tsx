2cc2026854d175e7a9226ff4e7054b0f591e0129
murilobarbosa2002 <murilo.barbosa2609@gmail.com>
2022-06-16 20:21:33 -0300

    Edit router.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { HomePage } from './screens/Home'
 import { DocsPage } from './screens/Docs'
 export function AppRoutes(){
